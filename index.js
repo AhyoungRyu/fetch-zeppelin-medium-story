61704242c065ba3d4472e1a6b29680255f92d7bc
AhyoungRyu <fbdkdud93@hanmail.net>
2017-02-07 00:11:00 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var AWS = require('aws-sdk')
+AWS.config.setPromisesDependency(require('bluebird'))
+var s3 = new AWS.S3(
+  {
+    signatureVersion: 'v4',
+    maxRetries: 3
+  }
+)
+
+var Feed = require('rss-to-json')
+var Promise = require('bluebird')
+var jsonFormat = require('json-format')
+
+const storyUri = 'https://medium.com/feed/apache-zeppelin-stories/'
+Feed = Promise.promisifyAll(Feed)
+
+exports.handler = (event, context, callback) => {
+  Feed.loadAsync(storyUri)
+    .then(function (rss) {
+      var config = {
+        type: 'space',
+        size: 2
+      }
+      var result = jsonFormat(rss, config)
+
+      return result
+    })
+    .then(function (result) {
+      const bucketName = 'apache-zeppelin'
+      const folderName = 'post/'
+      var fakeJsBody = 'var mediumPost = [' + result + ']'
+
+      var content = {
+        Bucket: bucketName,
+        Key: folderName + 'medium.js',
+        Body: fakeJsBody,
+        ACL: 'public-read'
+      }
+
+      var putObjectPromise = s3.putObject(content).promise()
+
+      putObjectPromise
+        .then(function (data) {
+          console.log(data)
+        })
+        .then(function () {
+          return context.callbackWaitsForEmptyEventLoop = false
+        })
+        .then(function () {
+          return callback(null, "Done")
+        })
+    })
+    .catch(function (error) {
+      console.error(error.message)
+    })
+}
---
+++ test.js
@@ -0,0 +1,48 @@
+var AWS = require('aws-sdk')
+AWS.config.setPromisesDependency(require('bluebird'))
+var s3 = new AWS.S3(
+  {
+    signatureVersion: 'v4',
+    maxRetries: 3
+  }
+)
+
+var Feed = require('rss-to-json')
+var Promise = require('bluebird')
+var jsonFormat = require('json-format')
+
+const storyUri = 'https://medium.com/feed/apache-zeppelin-stories/'
+Feed = Promise.promisifyAll(Feed)
+
+Feed.loadAsync(storyUri)
+  .then(function(rss) {
+    var config = {
+      type: 'space',
+      size: 2
+    }
+    var result = jsonFormat(rss, config)
+
+    return result
+  })
+  .then(function (result) {
+    const bucketName = 'apache-zeppelin'
+    const folderName = 'post/'
+    var fakeJsBody = 'var mediumPost = [' + result + ']'
+
+    var content = {
+      Bucket: bucketName,
+      Key: folderName + 'medium.js',
+      Body: fakeJsBody,
+      ACL: 'public-read'
+    }
+
+    var putObjectPromise = s3.putObject(content).promise()
+
+    putObjectPromise
+      .then(function (data) {
+        console.log(data)
+      })
+  })
+  .catch(function (error) {
+    console.error(error.message)
+  })
